c49ed4d477f59c623167d7be809c97a9f189ad6a
Brent Pabst <me@brentpabst.com>
2014-12-29 16:36:35 -0500

    OG meta updates

--- app/viewmodels/shell.js
+++ app/viewmodels/shell.js
@@ -23,9 +23,14 @@
         router: router,
         activate: function () {
             router.on("router:navigation:complete", function () {
+                // Home page heading
                 if (this.activeInstruction().config.hash === "#") {
                     showHeading(true);
                 } else showHeading(false);
+
+                // Meta tag updates/replacements
+                $("meta[name='og:title']").attr("content", document.title);
+                $("meta[name='og:url']").attr("content", location.toString());
 
                 //ga("send", { "hitType": "pageview", "page": this.activeInstruction().config.hash });
             });
@@ -34,7 +39,7 @@
                 { route: "", title: "Home", moduleId: "viewmodels/home", isListen: false, nav: true },
                 { route: "music", title: "Music", moduleId: "viewmodels/music", isListen: false, nav: false },
                 { route: "store", title: "Store", moduleId: "viewmodels/store", isListen: false, nav: false },
-                { route: "listen", title: "<i class=\"fa fa-headphones\"></i>&nbsp;&nbsp;Listen Online", isListen: true, moduleId: "viewmodels/listen", nav: true },
+                { route: "listen", title: "Listen Online", isListen: true, moduleId: "viewmodels/listen", nav: true },
                 { route: "download", title: "Download", moduleId: "viewmodels/download", isListen: false, nav: false },
                 { route: "legal", title: "Legal Stuff", moduleId: "viewmodels/legal", isListen: false, nav: false },
                 //{ route: "contact", title: "Contact", moduleId: "viewmodels/contact", nav: true }
