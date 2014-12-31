755be38847b6b1cf25da0842955c885a92e44162
Brent Pabst <me@brentpabst.com>
2014-12-30 22:20:30 -0500

    Player viewport size

--- app/viewmodels/listen.js
+++ app/viewmodels/listen.js
@@ -1,7 +1,7 @@
 ﻿define(["plugins/router"], function (router) {
 
     var openPlayer = function () {
-        window.open("http://www.streamlicensing.com/stations/fluffy/player.html", "Fluffy Radio", "status=0,toolbar=0,location=1,menubar=0,directories=0,resizable=0,scrollbars=0,width=300,height=340");
+        window.open("http://www.streamlicensing.com/stations/fluffy/player.html", "Fluffy Radio", "status=0,toolbar=0,location=1,menubar=0,directories=0,resizable=0,scrollbars=0,width=300,height=250");
     }
 
     return {
